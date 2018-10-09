1a862bd8e68c8a9f3514439ff2c3ed708168b107
unknown <samuel_newhouse@yahoo.com>
2018-10-09 14:45:27 -0500

    Stop here to go back. Need old margin method to avoid jitter.

--- scripts/diagonally-distributed-elements.js
+++ scripts/diagonally-distributed-elements.js
@@ -21,6 +21,8 @@
  *  dde-box (required):        sets up an element as the container for text
  *  dde-down (default):        run text on a downhill diagonal 
  *  dde-up:                    run text on an uphill diagonal
+ *  dde-left-block (default):  line up on left margin in extreme cases
+ *  dde-right-block:           line up on right margin in extreme cases
  * 
  * https://www.github.com/
  * Samuel Newhouse
@@ -40,38 +42,28 @@ document.addEventListener("DOMContentLoaded", function () {
     if (!boxes)
         return;
 
-    function boxResize(box) {        
+    function boxResize(box) {
         var parts = box.children;
         // Last child will be a div added from javascript-detect-element-resize.js.
         // It must be ignored for this to work properly.
-        var numParts = parts.length - 1; // <--------------------------------------            
+        var numParts = parts.length - 1; // <-----------
 
-        var halfSizes = [];
-        for (let i = 0; i < numParts; i++)
-            halfSizes[i] = parts[i].clientWidth / 2;
-
-        var fullWidth = box.clientWidth;
-        var leftMidpoint = halfSizes[0];
-        var rightMidpoint = fullWidth - halfSizes[numParts - 1];
+        var leftMidpoint = parts[0].clientWidth / 2;
+        var rightMidpoint = box.clientWidth - parts[numParts - 1].clientWidth / 2;
 
         var innerWidth = rightMidpoint - leftMidpoint;
         var splitWidth = innerWidth / (numParts - 1);
 
-        var leftOffset = leftMidpoint;
-        var rightOffset = fullWidth - rightMidpoint;
+        for (let i = 1; i < numParts; i++) {
+            let marginLeft = splitWidth * i + leftMidpoint - parts[i].clientWidth / 2;
 
-        var halfPartsIndex = Math.floor(numParts / 2);
-        for (let i = 1; i < halfPartsIndex; i++) {
-            let iMirror = numParts - i - 1;
-            let marginLeft = splitWidth * i + leftOffset - halfSizes[i];            
-            let marginRight = splitWidth * i + rightOffset - halfSizes[iMirror];
-            marginLeft = Math.max(0, marginLeft);
-            marginRight= Math.max(0, marginRight);
+            if (marginLeft < 0)
+                marginLeft = 0;
+            if (marginLeft > box.clientWidth - parts[i].clientWidth)
+                marginLeft = box.clientWidth - parts[i].clientWidth;
+
             parts[i].style.marginLeft = marginLeft + "px";
-            parts[iMirror].style.marginRight = marginRight + "px";
         }
-        if (numParts % 2 !== 0)
-            parts[halfPartsIndex].style.marginLeft = leftOffset + splitWidth * halfPartsIndex - halfSizes[halfPartsIndex] + "px";
     }
 
     for (let i = 0; i < boxes.length; i++) {
@@ -81,21 +73,19 @@ document.addEventListener("DOMContentLoaded", function () {
             boxResize(curBox);
         });
 
-        // flex is used for its align-items property.
+        if()
+        
         curBox.style.display = "flex";
         curBox.style.flexDirection = "column";
         curBox.style.alignItems = "flex-start";
-        curBox.style.textAlign = "center";
+        // If text wraps, this keeps it looking good
+        curBox.style.textAlign = "center"; // <-----
 
         var parts = curBox.children;
 
-        var halfPartsIndex = Math.floor(parts.length / 2);
+        for (let j = 0; j < parts.length; j++)
+            // Need width of container shrunk to content...
+            parts[j].style.display = "inline-block"; // <--
 
-        for (let j = 0; j < parts.length; j++) {
-            // Need width of container shrunk to content. <--
-            parts[j].style.display = "inline-block"; // <----
-            if (j >= halfPartsIndex)
-                parts[j].style.alignSelf = "flex-end";
-        }
     }
 });
