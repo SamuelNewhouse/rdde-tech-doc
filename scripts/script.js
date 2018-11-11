befda8830a6673ffe88a416e26482b7ff2578cd3
unknown <samuel_newhouse@yahoo.com>
2018-11-10 21:27:45 -0600

    Add JavaScript for tracking and showing page location in navbar links

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
+    "use strict";
     var sizeToggles = document.getElementsByClassName("size-toggle");
 
     for (let i = 0; i < sizeToggles.length; i++) {
@@ -24,11 +25,33 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("navbar").classList.add("closed");
         document.getElementById("close-navbar").classList.add("closed");
     }
+
     function openNavbar() {
         document.getElementById("navbar").classList.remove("closed");
         document.getElementById("close-navbar").classList.remove("closed");
     }
 
+    var lastNavFocus = null;
+    function checkNavFocus() {
+        var element = document.elementFromPoint(400, 30);
+
+        var curNavFocus = null;
+        // Move up through document tree until a section is found.
+        for (; element && element !== document; element = element.parentNode)
+            if (element.matches("section"))
+                curNavFocus = element.id;
+
+        if (curNavFocus && curNavFocus != lastNavFocus) {
+            if (lastNavFocus) {
+                let last = document.querySelector('.nav-link[href="#' + lastNavFocus + '"]');
+                last.classList.remove("navbar-focus");
+            }
+
+            let cur = document.querySelector('.nav-link[href="#' + curNavFocus + '"]');
+            cur.classList.add("navbar-focus");
+            lastNavFocus = curNavFocus;
+        }
+    }
 
     var lastWinWidth = 99999;
     function checkSize() {
@@ -44,7 +67,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
     document.getElementById("close-navbar").addEventListener("click", closeNavbar);
     document.getElementById("open-navbar").addEventListener("click", openNavbar);
+    window.addEventListener("scroll", checkNavFocus);
     window.addEventListener("resize", checkSize);
 
     checkSize();
+    checkNavFocus();
 });
